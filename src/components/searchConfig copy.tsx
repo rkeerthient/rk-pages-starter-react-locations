30e9940f76c4c08b80b7545c109b63e53add2fe4
rkeerthi <sunnykeerthi@gmail.com>
2023-06-08 15:10:15 +0530

    deleted search copy and updated env vars in config

--- src/components/searchConfig copy.tsx
+++
@@ -1,8 +0,0 @@
-const searchConfig = {
-  apiKey: "03278d77949095f1c0ac1fd0c020dd0e",
-  experienceKey: "answers",
-  locale: "en",
-  verticalKey: "products",
-};
-
-export default searchConfig;
--- src/components/searchConfig.tsx
+++ src/components/searchConfig.tsx
@@ -1,10 +1,10 @@
 // import { CloudRegion, Environment } from "@yext/search-headless-react";
 
 const searchConfig = {
-  apiKey: import.meta.env.API_KEY,
-  experienceKey: import.meta.env.EXP_KEY,
+  apiKey: import.meta.env.YEXT_PUBLIC_API_KEY,
+  experienceKey: import.meta.env.YEXT_PUBLIC_EXP_KEY,
   locale: "en",
-  verticalKey: "restaurants",
+  // verticalKey: "restaurants",
   // cloudRegion: CloudRegion.US,
   // environment: Environment.SANDBOX,
 };
